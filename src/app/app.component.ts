680de02ffd2a59bd41eca0ad041edeff0ba19ea8
anuj gangwal <anujgangwl@gmail.com>
2018-07-20 15:09:01 +0530

    drag&drop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {DropEvent} from 'ng-drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  appData = [
+    { name: 'SpreadSheet Name', type: 'sheet' },
+    { name: 'Row ID', type: 'sheet' },
+    { name: 'Row No', type: 'sheet' },
+    { name: 'Column', type: 'sheet' },
+    { name: 'col-1', type: 'column' },
+    { name: 'col-2', type: 'column' },
+    { name: 'col-3', type: 'column' },
+    { name: 'col-4', type: 'column' }
+]
+
+droppedSheetdata = [];
+droppedItems = [];
+dragEnabled = true;
+
+
+onSheetDrop(e: DropEvent) {
+  this.droppedSheetdata.push(e.dragData);
+  this.removeItem(e.dragData, this.appData);
+}
+
+onDrop(e: DropEvent) {
+  this.droppedItems.push(e.dragData);
+  this.removeItem(e.dragData, this.appData);    
+}
+
+removeItem(item: any, list: Array<any>) {
+  let index = list.map(function (e) {
+    return e.name
+  }).indexOf(item.name);
+  list.splice(index, 1);
+}
 }
